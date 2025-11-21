b57d4ceb678533e37075b4838326c10fd3e88906
Ankush25-anku <ankushshetty528@gmail.com>
2025-11-21 15:49:59 +0530

    Updated firebaseAdmin to use environment variables + backend fixes

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,8 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// Load Firebase service account from Vercel environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
